87dc4ab5e38c39d7304915f797a695bee9a25ca8
Carlos Rodriguez <carlos@s8f.org>
2013-07-15 17:21:05 -0700

    working

---
+++ example/server.js
@@ -0,0 +1,24 @@
+var app = require('motley')
+  , loginWithGithub = require('../')
+
+app.boot(function (err) {
+  if (err) throw err;
+  app.motley();
+
+  // here's where we pass options
+  var loginHandler = loginWithGithub(app.conf);
+
+  app.router
+    .get('/', app.dish('<a href="/login/github">login with github</a>'))
+    .get('/login/github', loginHandler, function (req, res, next) {
+      res.json(req.github_user);
+      // also req.github_access_token available
+    })
+    .on('error', function (err, req, res) {
+      res.json(500, err);
+    })
+
+  app.server.listen(app.conf.port, function () {
+    console.log('server started at http://localhost:' + app.server.address().port + '/');
+  });
+});
--- index.js
+++ index.js
@@ -11,26 +11,18 @@ module.exports = function (_opts) {
   });
 
   // required options
-  options.client_id || throw new Error('options.client_id requried');
-  options.client_secret || throw new Error('options.client_secret required');
+  if (!options.client_id) throw new Error('options.client_id requried');
+  if (!options.client_secret) throw new Error('options.client_secret required');
 
   // this is the path we'll mount handlers on
-  options.loginPath || (options.path = '/login/github');
+  options.login_path || (options.login_path = '/login/github');
 
   // github api endpoints
-  options.authorizeUrl || (options.authorizePath = 'https://github.com/login/oauth/authorize');
-  options.accessTokenUrl || (options.accessTokenPath = 'https://github.com/login/oauth/access_token');
-  options.userUrl || (options.userPath = 'https://api.github.com/user');
+  options.authorize_url || (options.authorize_url = 'https://github.com/login/oauth/authorize');
+  options.access_token_url || (options.access_token_url = 'https://github.com/login/oauth/access_token');
+  options.user_url || (options.user_url = 'https://api.github.com/user');
 
-  // default callbacks
-  options.onError || (options.onError = function (err, req, res, next) {
-    next(err);
-  });
-  options.onSuccess || (options.onSuccess = function (access_token, user, req, res, next) {
-    req.github_user = user;
-    req.github_access_token = access_token;
-    next();
-  });
+  if (typeof options.fetch_user === 'undefined') options.fetch_user = true;
 
   var controller = middler();
 
@@ -51,7 +43,7 @@ module.exports = function (_opts) {
       }
       next();
     })
-    .get(options.loginPath, function (req, res, next) {
+    .get(options.login_path, function (req, res, next) {
       if (req.query.code) processCode(req, res, next);
       else if (req.query.error) processError(req, res, next);
       else doRedirect(req, res, next);
@@ -61,55 +53,61 @@ module.exports = function (_opts) {
       next();
     })
 
+  function doRedirect (req, res, next) {
+    req.session.github_login_state = idgen(16);
+    req.session.github_redirect_uri = req.href.href;
+    var uri = options.authorize_url + '?';
+    uri += qs.stringify({
+      client_id: options.client_id,
+      redirect_uri: req.session.github_redirect_uri,
+      scope: options.scope,
+      state: req.session.github_login_state
+    });
+    res.redirect(uri);
+  }
+
   function processCode (req, res, next) {
     if (!req.session.github_login_state) return next(new Error('unknown login state'));
     if (!req.query.state) return next(new Error('state param required'));
     if (req.session.github_login_state !== req.query.state) return next(new Error('incorrect login state'));
 
     req.session.github_login_state = null;
-    var uri = options.accessTokenPath + '?';
+    var uri = options.access_token_url + '?';
     var params = {
-      client_id: app.conf.github_client_id,
+      client_id: options.client_id,
       redirect_uri: req.session.github_redirect_uri,
-      client_secret: app.conf.github_client_secret,
+      client_secret: options.client_secret,
       code: req.query.code
     };
     req.session.github_redirect_uri = null;
-    app.request({uri: uri, method: 'post', json: params}, function (err, resp, body) {
+    request({uri: uri, method: 'post', json: params}, function (err, resp, body) {
       if (err) return next(err);
       if (resp.statusCode !== 200) {
-        console.error('status from post access_token', resp.statusCode);
-        return res.renderStatus(resp.statusCode);
+        err = new Error('non-200 status from post access_token: ' + resp.statusCode);
+        err.response = body;
+        return next(err);
       }
-      var access_token = body.access_token;
-      app.request({uri: options.userUrl + '?access_token=' + access_token, json: true}, function (err, resp, body) {
-        if (err) return next(err);
-        if (resp.statusCode !== 200) {
-          err = new Error('non-200 status from get user: ' + resp.statusCode);
-          return next(err);
-        }
-        
-      });
+      req.github_access_token = body.access_token;
+      if (options.fetch_user) {
+        request({uri: options.user_url + '?access_token=' + req.github_access_token, json: true}, function (err, resp, body) {
+          if (err) return next(err);
+          if (resp.statusCode !== 200) {
+            err = new Error('non-200 status from get user: ' + resp.statusCode);
+            err.response = body;
+            return next(err);
+          }
+          req.github_user = body;
+          next();
+        });
+      }
+      else next();
     });
   }
 
   function processError (req, res, next) {
-    var err = new Error(req.query.error);
+    var err = new Error('error authenticating with github');
     err.code = req.query.error;
-    next();
-  }
-
-  function doRedirect (req, res, next) {
-    req.session.github_login_state = idgen(16);
-    req.session.github_redirect_uri = req.href.href;
-    var uri = '?';
-    uri += app.qs.stringify({
-      client_id: app.conf.github_client_id,
-      redirect_uri: req.session.github_redirect_uri,
-      scope: app.conf.github_scope,
-      state: req.session.github_login_state
-    });
-    res.redirect(uri);
+    next(err);
   }
 
   return controller.handler;
